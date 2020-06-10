abb1012fd363dc094efda996919701e696ee0170
Alecu Cristian <alecucristianionut@gmail.com>
2020-06-10 09:40:53 +0300

    lesson 4

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+var names = 'Cristi';
+
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun'
+        name: 'Shaun',
+        job: "Programmer",
+        website: 'http://www.google.com',
+        websiteTag: '<a href="http://www.google.com">Google</a>'
+    },
+    methods: {
+        greet: function(time){
+            return 'Good ' + time + ' ' + names;
+        }
     }
 })
